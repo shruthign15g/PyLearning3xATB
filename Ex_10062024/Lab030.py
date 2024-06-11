37b9ccff96d8cdac964eff05b315f6a24dff4c87
shruthi <shruthign15@gmail.com>
2024-06-11 01:34:01 -0400

    Day2- Variables and Data Types

---
+++ Ex_10062024/Lab030.py
@@ -0,0 +1,8 @@
+print(10 / 10)
+
+# Logical Operator
+
+x=10
+y=20
+print(x>y)
+print(x<y)
---
+++ Ex_10062024/Lab032.py
@@ -0,0 +1,8 @@
+# Ternary Operator
+pramod_marks = 90
+amit_marks = 97
+
+# X>Y -> Then print ("Pramod")
+# Y>X -> Then print ("Amit")
+
+print("pramod is winner" if pramod_marks > amit_marks else "amit is winner")
